9adb31656f398ba7d955f33c61d83b618064266b
Jag Bains <git@jagbains.org>
2025-01-15 10:44:37 +0000

    robot class created

--- day11/1.py
+++ day11/1.py
@@ -16,3 +16,28 @@ for line in f.readlines():
 program=intcode.intcode(code)
 
 print(program)
+
+class robot:
+    def __init__(self):
+        self.dirs={"^":{x:"0",y:"-1",r:">",l:"<"},">":{x:"1",y:"0",r:"v",l:"^"},"v":{x:"0",y:"1",r:"<",l:">"},"<":{x:"-1",y:"0",l:"v",r:"^"}}
+        self.path={"0.0":0}
+        self.dir="^"
+        self.cur="0.0"
+
+
+    def move(self,colour,turn):
+        self.path[self.cur]=colour
+        if turn==0:
+            #left
+            self.dir=self.dirs[self.dir][l]
+        elif turn==1:
+            self.dir=self.dirs[self.dir][r]
+        else:
+            raise ValueError("invalid direction",turn)
+        coords=self.cur.split(".")
+        x=int(coords[0])+int(self.dirs[self.dir][x])
+        y=int(coords[1])+int(self.dirs[self.dir][y])
+        self.cur=str(x)+"."+str(y)
+        if self.cur not in self.path:
+            self.path[self.cur]=0
+        return self.path[self.cur]
