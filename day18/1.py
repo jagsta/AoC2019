55c8c0156897cba33911535f44d56b67637972ee
Jag Bains <git@jagbains.org>
2025-01-21 12:29:56 +0000

    oh my, this does not scale well

--- day18/1.py
+++ day18/1.py
@@ -12,6 +12,7 @@ G=nx.Graph()
 
 origin=""
 keys={}
+locks={}
 gdict={}
 y=0
 x=0
@@ -21,6 +22,7 @@ for line in f.readlines():
         if c!="#":
             if c.isupper():
                 print("Lock",c,"at",x,y)
+                locks[str(x)+"."+str(y)]=c
                 G.add_node(str(x)+"."+str(y),char=c,isLock=True)
             elif c.islower():
                 print("key",c,"at",x,y)
@@ -30,14 +32,12 @@ for line in f.readlines():
                 print("origin at",x,y)
                 origin=str(x)+"."+str(y)
             for d in dirs:
-                if c.isupper():
-                    break
                 print("trying dir",d)
-                nx=x+d[0]
-                ny=y+d[1]
-                if str(nx)+"."+str(ny) in gdict and gdict[str(nx)+"."+str(ny)]!="#" and gdict[str(nx)+"."+str(ny)].isupper() is False:
+                newx=x+d[0]
+                newy=y+d[1]
+                if str(newx)+"."+str(newy) in gdict and gdict[str(newx)+"."+str(newy)]!="#":
                     print(x,y,"is adjacent to a space")
-                    G.add_edge(str(x)+"."+str(y),str(nx)+"."+str(ny))
+                    G.add_edge(str(x)+"."+str(y),str(newx)+"."+str(newy))
 
         x+=1
     x=0
@@ -48,3 +48,57 @@ print(gdict)
 print(G.nodes(data=True))
 print(G.edges)
 print(keys)
+keysleft=dict(keys)
+locksleft=dict(locks)
+
+def permute(orig,locksleft,keysleft,length,ph):
+    global tried
+    tried+=1
+    if tried%100000==0:
+        print(f'tried {tried} paths')
+    global shortest
+    if shortest < length:
+#        print("Already too far, killing this path")
+        return 0
+    for key in keysleft:
+            thisl=length
+            thisph=ph
+#        try:
+#            print(f'trying from {orig} to {keys[key]} at {key}, length is {length} and path is {ph}')
+            path=nx.shortest_path(G,source=orig,target=key)
+#            print(f'path is {path}')
+            blocked=False
+            for p in path:
+                if p in locksleft:
+#                    print(f'{p} is a key in locksleft, so blocked')
+                    blocked=True
+                    break
+            if not blocked:
+                thisph+=keys[key]
+                thisl+=len(path)-1
+#                print(orig,key,keys[key],len(path),thisl)
+                k=dict(keysleft)
+                k.pop(key)
+#                print(f'{len(k)} keys left: {k}')
+                l=dict(locksleft)
+                for coord,lock in l.items():
+#                    print(f'trying {keys[key].upper()} and {lock}')
+                    if keys[key].upper()==lock:
+                        l.pop(coord)
+                        break
+                if len(k)>0:
+                    thisl+=permute(key,l,k,thisl,thisph)
+                else:
+#                    print(f'final distance was {thisl} for path {thisph}')
+                    ph=""
+                    if thisl<shortest:
+                        shortest=thisl
+#        except Exception as error:
+#            print("An exception occurred:", error)
+#            continue
+    return 0
+tried=0
+shortest=10000000
+print("shortest paths:")
+permute(origin,locksleft,keysleft,0,"")
+print(shortest)
