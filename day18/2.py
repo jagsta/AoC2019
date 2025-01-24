ed1b3283ad4edb1fd72e42ec7de491a25f79e608
Jag Bains <git@jagbains.org>
2025-01-24 10:28:36 +0000

    part2 complete!

--- day18/2.py
+++ day18/2.py
@@ -1,3 +1,21 @@
+#Thoughts on how to approach this
+# I need to iterate over the 4 robots to recheck whether they have a move possible
+# I need to maintain a unique key bitmask which I pass to subsequent moves (whether of my robot or another of the 4)
+
+# Peudo code:
+# Add all 4 robots initial positions to the stack
+# pop a state off the stack ( robot 1 initially)
+# Repeat while there's state to process:
+#
+# Do we have all the keys?
+#   If so, is the score lower than previous best?
+#       If so, update best to this score
+#           Bail this state
+# Do we already have a shorter distance for this position/keymap combo?
+#   if so, bail?
+# Any valid moves? If yes, add each to the back of the stack with state (moves + keymask)
+#   If a move available, check if any valid moves for the other 3 robots with that state, and add to the back of the stack
+# Maybe try first with a single measure of moves(sum across any robot), this may work, as keymaps will vary
 import sys
 import itertools
 import networkx as nx
@@ -102,69 +120,63 @@ print(G.edges)
 
 #This doesnt' work, try using restricted_view with list of nodes supplied to suppress based on isLocked dict copies?
 
-def permute(origins):
+def permute(oid1,oid2,oid3,oid4,length,key):
     global tried
     global pruned
-    global shortest
-    global best
-    origid=1
-    for orig,length,key in origins:
-        best_ds[(orig,key)]=0
-        heap.append((orig,length,key,origid))
-        origid+=1
-    final_dist=defaultdict(lambda:10000000)
-    gkey=0
+    best_ds[(oid1,oid2,oid3,oid4,key)]=0
+    heap.append((oid1,oid2,oid3,oid4,length,key))
+    final_dist=inf
     if tried%10000==0:
         print(f'tried {tried} paths and pruned {pruned}, current best path is {best} {shortest}')
     while len(heap)>0:
         # take the oldest tuple off the stack
-        o,l,k,oid = heap.popleft()
-        k=gkey
-        final_dist[(oid,o,k)]=l
-        if len(heap)==0 and len(backheap)>1:
-            while True:
-                print('replenishing heap')
-                heap.append(backheap.popleft())
-                if len(backheap)==0:
-                    break
-        # Is this a tuple with all keys?
-        if k==fullmap:
-            #is it the best distance we've seen
-            if best_ds[(o,k)]<final_dist[oid]:
-                final_dist[oid]=best_ds[(o,k)]
-                continue
+        o1,o2,o3,o4,l,k = heap.popleft()
         #Is this already worse than the best we've seen for this node with these keys?
-        if l>best_ds[(o,k)]:
+        if best_ds[(o1,o2,o3,o4,k)]<l:
             #print("Already too far, killing this path")
             pruned+=1
             continue
-        # for next hops from this node(o)
-        stuck=False
-        for node in (P.neighbors(o)):
-            print(oid,o,node,l,bin(k))
-            thisk=k
-            if best_ds[(node,k)]<=l:
-                pruned+=1
+        # Is this a tuple with all keys?
+        if k==fullmap:
+            #is it the best distance we've seen
+            if best_ds[(o1,o2,o3,o4,k)]<final_dist:
+                final_dist=best_ds[(o1,o2,o3,o4,k)]
                 continue
-            # If lock and we don't have the key, abandon
-            if node.isupper() and not bm2k(node.lower(),thisk):
-                stuck=True
-                continue
-            # If key and we don't have it yet, add the key to mask
-            if node.islower() and node[0]!='@' and not bm2k(node,thisk):
-                print(bin(thisk))
-                thisk=k2bm(node,thisk)
-                stuck=False
-            thisl=l+plengths[(o,node)]
-            if best_ds[(node,thisk)]<thisl:
-                continue
-            else:
-                best_ds[(node,thisk)]=thisl
-                heap.append((node,thisl,thisk,oid))
-                gkey=thisk
-        if stuck==True:
-            #print(f'{oid} is stuck, adding to back of queue')
-            backheap.append((o,l,k,oid))
+        # for each robot
+        for o in [o1,o2,o3,o4]:
+            # for next hops from this node(o)
+            for node in pneighbors[o]:
+                thisk=k
+                #if best_ds[(node,k)]<=l:
+                #    pruned+=1
+                #    continue
+                # If lock and we don't have the key, abandon
+                if node.isupper() and not bm2k(node.lower(),thisk):
+                    continue
+                # If key and we don't have it yet, add the key to mask
+                if node.islower() and node[0]!='@' and not bm2k(node,thisk):
+                    #print(bin(thisk))
+                    thisk=k2bm(node,thisk)
+                thisl=l+plengths[(o,node)]
+                if o==o1:
+                    t=(node,o2,o3,o4,thisk)
+                    h=(node,o2,o3,o4,thisl,thisk)
+                elif o==o2:
+                    t=(o1,node,o3,o4,thisk)
+                    h=(o1,node,o3,o4,thisl,thisk)
+                elif o==o3:
+                    t=(o1,o2,node,o4,thisk)
+                    h=(o1,o2,node,o4,thisl,thisk)
+                elif o==o4:
+                    t=(o1,o2,o3,node,thisk)
+                    h=(o1,o2,o3,node,thisl,thisk)
+                if best_ds[t]<=thisl:
+                    continue
+                else:
+                    #print(h,bin(thisk))
+                    best_ds[t]=thisl
+                    heap.append(h)
+
 
     return final_dist
 
@@ -202,13 +214,18 @@ for a,b in P.edges:
 
 print(plengths)
 
+pneighbors={}
+for a in P.nodes:
+    pneighbors[a]=set(P.neighbors(a))
+
+print(pneighbors)
+
 tried=0
 pruned=0
 shortest=0
 best=""
 best_ds=defaultdict(lambda: inf)
-shortest=permute([("@1",0,0),("@2",0,0),("@3",0,0),("@4",0,0)])
-for i in shortest:
-    print(i,shortest[i])
+shortest=permute("@1","@2","@3","@4",0,0)
+print(shortest)
 print(f'tried {tried}')
 print(f'pruned {pruned}')
