1e8c0b6bfd00c5cf571a77bc2f14ea52b272b4fe
Jag Bains <git@jagbains.org>
2025-01-31 09:40:02 +0000

    first cut for part 2

--- day24/2.py
+++ day24/2.py
@@ -4,12 +4,13 @@ file="input.txt"
 
 f=open(file)
 
-grid={}
+gridlayers={}
 y=0
 x=0
+gridlayers[0]=gridlayer(0)
 for line in f.readlines():
     for c in line.strip():
-        grid[(x,y)]=c
+        gridlayers[0][(x,y)]=c
         x+=1
     y+=1
     x=0
@@ -22,13 +23,70 @@ class gridlayer:
     def getadj(self,coord):
         x=coord[0]
         y=coord[1]
+        adj=0
         if x==0 or x==4 or y==0 or y==4:
             #outside edges, so adjacencies in layer -1
             layer=self.layer-1
-        if 1<x<4 and 1<y<4:
+            if layer not in gridlayers:
+                gridlayers[layer]=gridlayer(layer)
+            for c in ((1,0),(0,1),(-1,0),(0,-1)):
+                if x+c[0]>4:
+                    if gridlayers[layer][(2,3)]=="#":
+                        adj+=1
+                elif x+c[0]<0:
+                    if gridlayers[layer][(2,1)]=="#":
+                        adj+=1
+                elif y+c[1]>4:
+                    if gridlayers[layer][(3,2)]=="#":
+                        adj+=1
+                elif y+c[1]<0:
+                    if gridlayers[layer][(1,2)]=="#":
+                        adj+=1
+                elif gridlayers[(y+c[0],x+c[1])]=="#":
+                        adj+=1
+
+        if 0<x<4 and 0<y<4:
             #adjacent to centre square, so adjacencies in layer +1
             layer=self.layer+1
+            if layer not in gridlayers:
+                gridlayers[layer]=gridlayer(layer)
+            for c in ((1,0),(0,1),(-1,0),(0,-1)):
+                if x+c[0]==2:
+                    if y+c[1]==1:
+                        ny=0
+                    elif y+c[1]==3:
+                        ny=4
+                    for nx in range(5):
+                        if gridlayers[layer][(ny,nx)]=="#":
+                            adj+=1
+                elif y+c[1]==2:
+                    if x+c[0]==1:
+                        nx=0
+                    elif x+c[0]==3:
+                        nx=4
+                    for ny in range(5):
+                        if gridlayers[layer][(ny,nx)]=="#":
+                            adj+=1
+                elif gridlayer[(y+c[0],x+c[1])]=="#":
+                    adj+=1
+        return adj
 
+    def next(self,current,count):
+        if current=="#" and count!=1:
+            return "."
+        elif current=="." and (count==1 or count==2):
+            return "#"
+        else:
+            return current
+
+    def calcstate(self):
+        new={}
+        for layer in gridlayers:
+            new[layer]=gridlayer(layer)
+            for coord,current in gridlayers[layer].items():
+                count=getadj(current)
+                new[layer][coord]=next(layer[coord],count)
+        return new
 
 
 print(grid)
@@ -40,46 +98,20 @@ def adj(coord):
             count+=1
     return count
 
-def recadj(coord):
-    count=0
-    for a in [(1,0),(0,-1),(-1,0),(0,1)]:
-        if (coord[0]+a[0],coord[1]+a[1]) in grid and grid[(coord[0]+a[0],coord[1]+a[1])]=="#":
-            count+=1
-    return count
-
-
-def next(current,count):
-    if current=="#" and count!=1:
-        return "."
-    elif current=="." and (count==1 or count==2):
-        return "#"
-    else:
-        return current
-
-def calcstate(grid):
-    score=0
-    for coord,current in grid.items():
-        if current=="#":
-            score+=2**((coord[1])*5+coord[0])
-    return score
-
 states=set()
 minutes=0
 
-layers={}
-
 while True:
-    new={}
-    for coord,current in grid.items():
-        count=adj(coord)
-        new[coord]=next(current,count)
-    state=calcstate(new)
-    if state in states:
-        print (f'repeated state observed, with score {state}:\n{new}')
-        break
-    else:
-        states.add(state)
-    grid=new.copy()
+    gridlayers=gridlayers[0].calcstate()
     minutes+=1
+    if minutes=200:
+        total=0
+        for layer in gridlayers.values():
+            for current in layer.values():
+                if current=="#":
+                    total+=1
+        print(f'total bugs at minute {minute}: {total}')
+        break
+
 
 
